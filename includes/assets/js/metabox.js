23f1a5e12812f2c9e76d6a12144e1487399612e6
Thoriq Firdaus <tfirdaus@outlook.com>
2017-06-01 18:30:00 +0700

    refactor: for more clarity

--- includes/assets/js/metabox.js
+++ includes/assets/js/metabox.js
@@ -19,30 +19,8 @@ window.nineCodesMetabox = window.nineCodesMetabox || {};
 	 * @var {Object}
 	 */
 	var api = nineCodesMetabox = {
-
-			/**
-			 * Houses the manager, section, and control views based on the `type`.
-			 *
-			 * @since 0.1.0
-			 * @var {Object}
-			 */
-			metabox: {
-				manager: {},
-				section: {},
-				control: {},
-				nav: {},
-			},
-
-			/**
-			 * Houses the manager, section, and control templates based on the `type`.
-			 *
-			 * @since  0.1.0
-			 * @var {Object}
-			 */
+			metabox: {},
 			template: {
-				manager: {},
-				section: {},
-				control: {},
 				nav: wp.template('ninecodes-metabox-nav'),
 			}
 		},
@@ -133,7 +111,7 @@ window.nineCodesMetabox = window.nineCodesMetabox || {};
 				/**
 				 * Renders the Section
 				 *
-				 * @returns {Object} api.metabox.section
+				 * @returns {Object} section
 				 */
 				render: function () {
 
@@ -161,348 +139,374 @@ window.nineCodesMetabox = window.nineCodesMetabox || {};
 		};
 
 	/**
-	 * Creates a new Manager view
+	 * Houses the manager, section, and control views based on the `type`.
 	 *
 	 * @since 0.1.0
-	 *
-	 * @param {String} type Manager type.
-	 * @param {Object} args Manager arguments.
-	 * @return {Void} Returns nothing.
+	 * @var {Object}
 	 */
-	api.metabox.registerManager = function (type, args) {
+	api.metabox = {
 
-		if ('default' !== type) {
-			this.manager[type] = this.manager.default.extend(args);
-		}
+		manager: [],
+		section: [],
+		control: [],
+
+		/**
+		 * Creates a new Manager view
+		 *
+		 * @since 0.1.0
+		 *
+		 * @param {String} type Manager type.
+		 * @param {Object} args Manager arguments.
+		 * @return {Void} Returns nothing.
+		 */
+		registerManager: function (type, args) {
+
+			if ('default' !== type) {
+				this.manager[type] = this.manager.default.extend(args);
+			}
+		},
+
+		/**
+		 * Returns a Manager view
+		 *
+
+		* @since 0.1.0
+		*
+		* @param {String} type Manager type.
+		* @return {Object} The Manager instance.
+		*/
+		getManager: function (type) {
+
+			if (this.managerExists(type)) {
+				return this.manager[type];
+			}
+
+			return this.manager.default;
+		},
+
+		/**
+		 * Removes a manager view.
+		 *
+		 * @since 0.1.0
+		 *
+		 * @param {String} type Manager type.
+		 * @return {Void} Returns nothing.
+		 */
+		unregisterManager: function (type) {
+
+			if ('default' !== type && this.managerExists(type)) {
+				delete this.manager[type];
+			}
+		},
+
+		/**
+		 * Checks if a Manager view exists
+		 *
+		 * @since 0.1.0
+		 *
+		 * @param {String} type Manager type.
+		 * @return {Boolean} Returns `true` if the Manager of the given type exists.
+		 */
+		managerExists: function (type) {
+
+			return this.manager.hasOwnProperty(type);
+		},
+
+		/**
+		 * Creates a new Section view
+		 *
+		 * @since 0.1.0
+		 *
+		 * @param {String} type The new Section type to register.
+		 * @param {Object} args The Section arguments.
+		 * @return {Void} Returns nothing.
+		 */
+		registerSection: function (type, args) {
+
+			if ('default' !== type) {
+				this.section[type] = this.section.default.extend(args);
+			}
+		},
+
+		/**
+		 * Returns a Section view
+		 *
+		 * @since 0.1.0
+		 *
+		 * @param {String} type Section type.
+		 * @return {Object} Section instance.
+		 */
+		getSection: function (type) {
+
+			if (this.sectionExists(type)) {
+				return this.section[type];
+			}
+
+			return this.section.default;
+		},
+
+		/**
+		 * Removes a Section view
+		 *
+		 * @since  0.1.0
+		 *
+		 * @param {String} type Section type.
+		 * @return {Void} Returns nothing.
+		 */
+		unregisterSection: function (type) {
+
+			if ('default' !== type && this.sectionExists(type)) {
+				delete this.section[type];
+			}
+		},
+
+		/**
+		 * Checks if a Section view exists
+		 *
+		 * @since 0.1.0
+		 *
+		 * @param {String} type Section type.
+		 * @return {Boolean} Returns `true` if the Section exists.
+		 */
+		sectionExists: function (type) {
+			return this.section.hasOwnProperty(type);
+		},
+
+		/**
+		 * Creates a new Control view
+		 *
+		 * @since 0.1.0
+		 *
+		 * @param {String} type A new Control type to register.
+		 * @param {Object} args Control arguments.
+		 * @return {Void} Returns nothing.
+		 */
+		registerControl: function (type, args) {
+
+			if ('default' !== type) {
+				this.control[type] = this.control.default.extend(args);
+			}
+		},
+
+		/**
+		 * Returns a Control view
+		 *
+		 * @since 0.1.0
+		 *
+		 * @param {String} type Control type.
+		 * @return {Object} Control instance.
+		 */
+		getControl: function (type) {
+
+			if (this.controlExists(type)) {
+				return this.control[type];
+			}
+
+			return this.control.default;
+		},
+
+		/**
+		 * Removes a Control view
+		 *
+		 * @since 0.1.0
+		 *
+		 * @param {String} type Control type.
+		 * @return {Void} Returns nothing.
+		 */
+		unregisterControl: function (type) {
+
+			if ('default' !== type && this.controlExists(type)) {
+				delete this.control[type];
+			}
+		},
+
+		/**
+		 * Checks if a Control view exists
+		 *
+		 * @since 0.1.0
+		 *
+		 * @param {String} type Control type.
+		 * @return {Boolean} Returns `true` if the Control of the given type exists.
+		 */
+		controlExists: function (type) {
+
+			return this.control.hasOwnProperty(type);
+		},
 	};
 
 	/**
-	 * Returns a Manager view
-	 *
-
-	 * @since 0.1.0
-	 *
-	 * @param {String} type Manager type.
-	 * @return {Object} The Manager instance.
-	 */
-	api.metabox.getManager = function (type) {
-
-		if (this.managerExists(type)) {
-			return this.manager[type];
-		}
-
-		return this.manager.default;
-	};
-
-	/**
-	 * Removes a manager view.
-	 *
-	 * @since 0.1.0
-	 *
-	 * @param {String} type Manager type.
-	 * @return {Void} Returns nothing.
-	 */
-	api.metabox.unregisterManager = function (type) {
-
-		if ('default' !== type && this.managerExists(type)) {
-			delete this.manager[type];
-		}
-	};
-
-	/**
-	 * Checks if a Manager view exists
-	 *
-	 * @since 0.1.0
-	 *
-	 * @param {String} type Manager type.
-	 * @return {Boolean} Returns `true` if the Manager of the given type exists.
-	 */
-	api.metabox.managerExists = function (type) {
-
-		return this.manager.hasOwnProperty(type);
-	};
-
-	/**
-	 * Creates a new Section view
-	 *
-	 * @since 0.1.0
-	 *
-	 * @param {String} type The new Section type to register.
-	 * @param {Object} args The Section arguments.
-	 * @return {Void} Returns nothing.
-	 */
-	api.metabox.registerSection = function (type, args) {
-
-		if ('default' !== type) {
-			this.section[type] = this.section.default.extend(args);
-		}
-	};
-
-	/**
-	 * Returns a Section view
-	 *
-	 * @since 0.1.0
-	 *
-	 * @param {String} type Section type.
-	 * @return {Object} Section instance.
-	 */
-	api.metabox.getSection = function (type) {
-
-		if (this.sectionExists(type)) {
-			return this.section[type];
-		}
-
-		return this.section.default;
-	};
-
-	/**
-	 * Removes a Section view
+	 * Houses the manager, section, and control templates based on the `type`.
 	 *
 	 * @since  0.1.0
-	 *
-	 * @param {String} type Section type.
-	 * @return {Void} Returns nothing.
+	 * @var {Object}
 	 */
-	api.metabox.unregisterSection = function (type) {
+	api.template = {
 
-		if ('default' !== type && this.sectionExists(type)) {
-			delete this.section[type];
-		}
-	};
+		manager: [],
+		section: [],
+		control: [],
 
-	/**
-	 * Checks if a Section view exists
-	 *
-	 * @since 0.1.0
-	 *
-	 * @param {String} type Section type.
-	 * @return {Boolean} Returns `true` if the Section exists.
-	 */
-	api.metabox.sectionExists = function (type) {
-		return this.section.hasOwnProperty(type);
-	};
+		/**
+		 * Creates a new Manager template
+		 *
+		 * @since 0.1.0
+		 *
+		 * @param {String} type Manager type.
+		 * @return {Void} Returns nothing.
+		 */
+		registerManager: function (type) {
 
-	/**
-	 * Creates a new Control view
-	 *
-	 * @since 0.1.0
-	 *
-	 * @param {String} type A new Control type to register.
-	 * @param {Object} args Control arguments.
-	 * @return {Void} Returns nothing.
-	 */
-	api.metabox.registerControl = function (type, args) {
+			this.manager[type] = wp.template('ninecodes-metabox-manager-' + type);
+		},
 
-		if ('default' !== type) {
-			this.control[type] = this.control.default.extend(args);
-		}
-	};
+		/**
+		 * Returns a Manager template
+		 *
+		 * @since 0.1.0
+		 *
+		 * @param {String} type Manager type.
+		 * @return {Function} A function that lazily-compiles the template requested.
+		 */
+		getManager: function (type) {
 
-	/**
-	 * Returns a Control view
-	 *
-	 * @since 0.1.0
-	 *
-	 * @param {String} type Control type.
-	 * @return {Object} Control instance.
-	 */
-	api.metabox.getControl = function (type) {
+			return this.managerExists(type) ? this.manager[type] : false;
+		},
 
-		if (this.controlExists(type)) {
-			return this.control[type];
-		}
+		/**
+		 * Removes a manager template
+		 *
+		 * @since 0.1.0
+		 *
+		 * @param {String} type Manager type.
+		 * @return {Void} Returns nothing.
+		 */
+		unregisterManager: function (type) {
 
-		return this.control.default;
-	};
+			if (this.managerExists(type)) {
+				delete this.manager[type];
+			}
+		},
 
-	/**
-	 * Removes a Control view
-	 *
-	 * @since 0.1.0
-	 *
-	 * @param {String} type Control type.
-	 * @return {Void} Returns nothing.
-	 */
-	api.metabox.unregisterControl = function (type) {
+		/**
+		 * Checks if a Manager template exists
+		 *
+		 * @since 0.1.0
+		 *
+		 * @param {String} type Manager type.
+		 * @return {Boolean} Returns true if the Manager template of the given type exists.
+		 */
+		managerExists: function (type) {
 
-		if ('default' !== type && this.controlExists(type)) {
-			delete this.control[type];
-		}
-	};
+			return this.manager.hasOwnProperty(type);
+		},
 
-	/**
-	 * Checks if a Control view exists
-	 *
-	 * @since 0.1.0
-	 *
-	 * @param {String} type Control type.
-	 * @return {Boolean} Returns `true` if the Control of the given type exists.
-	 */
-	api.metabox.controlExists = function (type) {
+		/**
+		 * Creates a new Section template
+		 *
+		 * @since 0.1.0
+		 *
+		 * @param {String} type Section type.
+		 * @return {Void} Returns nothing.
+		 */
+		registerSection: function (type) {
 
-		return this.control.hasOwnProperty(type);
-	};
+			this.section[type] = wp.template('ninecodes-metabox-section-' + type);
+		},
 
-	/**
-	 * Creates a new Manager template
-	 *
-	 * @since 0.1.0
-	 *
-	 * @param {String} type Manager type.
-	 * @return {Void} Returns nothing.
-	 */
-	api.template.registerManager = function (type) {
+		/**
+		 * Returns a Section template
+		 *
+		 * @since 0.1.0
+		 *
+		 * @param {String} type Manager type.
+		 * @return {Function} A function that lazily-compiles the template requested.
+		 */
+		getSection: function (type) {
 
-		this.manager[type] = wp.template('ninecodes-metabox-manager-' + type);
-	};
+			return this.sectionExists(type) ? this.section[type] : false;
+		},
 
-	/**
-	 * Returns a Manager template
-	 *
-	 * @since 0.1.0
-	 *
-	 * @param {String} type Manager type.
-	 * @return {Function} A function that lazily-compiles the template requested.
-	 */
-	api.template.getManager = function (type) {
+		/**
+		 * Removes a Section template
+		 *
+		 * @since 0.1.0
+		 *
+		 * @param {String} type Section type
+		 * @return {Void} Returns nothing.
+		 */
+		unregisterSection: function (type) {
 
-		return this.managerExists(type) ? this.manager[type] : false;
-	};
+			if (this.sectionExists(type)) {
+				delete this.section[type];
+			}
+		},
 
-	/**
-	 * Removes a manager template
-	 *
-	 * @since 0.1.0
-	 *
-	 * @param {String} type Manager type.
-	 * @return {Void} Returns nothing.
-	 */
-	api.template.unregisterManager = function (type) {
+		/**
+		 * Checks if a Section template exists
+		 *
+		 * @since 0.1.0
+		 *
+		 * @param {String} type Section type.
+		 * @return {Boolean} Returns `true` if the Section template of the given type exists.
+		 */
+		sectionExists: function (type) {
 
-		if (this.managerExists(type)) {
-			delete this.manager[type];
-		}
-	};
+			return this.section.hasOwnProperty(type);
+		},
 
-	/**
-	 * Checks if a Manager template exists
-	 *
-	 * @since 0.1.0
-	 *
-	 * @param {String} type Manager type.
-	 * @return {Boolean} Returns true if the Manager template of the given type exists.
-	 */
-	api.template.managerExists = function (type) {
+		/**
+		 * Creates a new Control template
+		 *
+		 * @since 0.1.0
+		 *
+		 * @param {String} type Control type.
+		 * @return {Void} Returns nothing.
+		 */
+		registerControl: function (type) {
 
-		return this.manager.hasOwnProperty(type);
-	};
+			this.control[type] = wp.template('ninecodes-metabox-control-' + type);
+		},
 
-	/**
-	 * Creates a new Section template
-	 *
-	 * @since 0.1.0
-	 *
-	 * @param {String} type Section type.
-	 * @return {Void} Returns nothing.
-	 */
-	api.template.registerSection = function (type) {
+		/**
+		 * Returns a Control template
+		 *
+		 * @since 0.1.0
+		 *
+		 * @param {String} type Control type.
+		 * @return {Function} A function that lazily-compiles the template requested.
+		 */
+		getControl: function (type) {
 
-		this.section[type] = wp.template('ninecodes-metabox-section-' + type);
-	};
+			return this.controlExists(type) ? this.control[type] : false;
+		},
 
-	/**
-	 * Returns a Section template
-	 *
-	 * @since 0.1.0
-	 *
-	 * @param {String} type Manager type.
-	 * @return {Function} A function that lazily-compiles the template requested.
-	 */
-	api.template.getSection = function (type) {
+		/**
+		 * Removes a control template.
+		 *
+		 * @since 0.1.0
+		 *
+		 * @param {String} type Control type.
+		 * @return {Void} Returns nothing.
+		 */
+		unregisterControl: function (type) {
 
-		return this.sectionExists(type) ? this.section[type] : false;
-	};
+			if (this.controlExists(type)) {
+				delete this.control[type];
+			}
+		},
 
-	/**
-	 * Removes a Section template
-	 *
-	 * @since 0.1.0
-	 *
-	 * @param {String} type Section type
-	 * @return {Void} Returns nothing.
-	 */
-	api.template.unregisterSection = function (type) {
+		/**
+		 * Checks if a Control template exists.
+		 *
+		 * @since 0.1.0
+		 *
+		 * @param {String} type Control type.
+		 * @return {Boolean} Returns `true` if the Control template of the given type exists.
+		 */
+		controlExists: function (type) {
 
-		if (this.sectionExists(type)) {
-			delete this.section[type];
-		}
-	};
-
-	/**
-	 * Checks if a Section template exists
-	 *
-	 * @since 0.1.0
-	 *
-	 * @param {String} type Section type.
-	 * @return {Boolean} Returns `true` if the Section template of the given type exists.
-	 */
-	api.template.sectionExists = function (type) {
-
-		return this.section.hasOwnProperty(type);
-	};
-
-	/**
-	 * Creates a new Control template
-	 *
-	 * @since 0.1.0
-	 *
-	 * @param {String} type Control type.
-	 * @return {Void} Returns nothing.
-	 */
-	api.template.registerControl = function (type) {
-
-		this.control[type] = wp.template('ninecodes-metabox-control-' + type);
-	};
-
-	/**
-	 * Returns a Control template
-	 *
-	 * @since 0.1.0
-	 *
-	 * @param {String} type Control type.
-	 * @return {Function} A function that lazily-compiles the template requested.
-	 */
-	api.template.getControl = function (type) {
-
-		return this.controlExists(type) ? this.control[type] : false;
-	};
-
-	/**
-	 * Removes a control template.
-	 *
-	 * @since 0.1.0
-	 *
-	 * @param {String} type Control type.
-	 * @return {Void} Returns nothing.
-	 */
-	api.template.unregisterControl = function (type) {
-
-		if (this.controlExists(type)) {
-			delete this.control[type];
-		}
-	};
-
-	/**
-	 * Checks if a Control template exists.
-	 *
-	 * @since 0.1.0
-	 *
-	 * @param {String} type Control type.
-	 * @return {Boolean} Returns `true` if the Control template of the given type exists.
-	 */
-	api.template.controlExists = function (type) {
-
-		return this.control.hasOwnProperty(type);
+			return this.control.hasOwnProperty(type);
+		},
 	};
 
 	/**
@@ -526,8 +530,8 @@ window.nineCodesMetabox = window.nineCodesMetabox || {};
 			// Add the `.ninecodes-metabox-ui` class to the meta box.
 			$('#ninecodes-metabox-ui-' + ManagerModel.get('name'))
 				.addClass('ninecodes-metabox-ui')
-					.find('.inside')
-						.append(ManagerView.render().el);
+				.find('.inside')
+				.append(ManagerView.render().el);
 
 			ManagerView.subViewRender();
 			ManagerView.ready();
@@ -794,7 +798,7 @@ window.nineCodesMetabox = window.nineCodesMetabox || {};
 		 *
 		 * @since 0.1.0
 		 *
-		 * @returns {Object} api.metabox.control
+		 * @returns {Object} control
 		 */
 		render: function () {
 
